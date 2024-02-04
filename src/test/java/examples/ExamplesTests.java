a72dceba3022218b5f81e23e03d8105384904bc1
Benjamin Bischoff <benjamin.bischoff@gmx.de>
2024-02-04 17:53:44 +0100

    first commit

---
+++ src/test/java/examples/ExamplesTests.java
@@ -0,0 +1,20 @@
+package examples;
+
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class ExamplesTests {
+
+    @Test
+    void testParallel() {
+        Results results = Runner.path("classpath:examples")
+            .outputHtmlReport(true)
+            .outputCucumberJson(true)
+            .parallel(1);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+    }
+
+}
---
+++ src/test/java/examples/test01/UiRunner.java
@@ -0,0 +1,12 @@
+package examples.test01;
+
+import com.intuit.karate.junit5.Karate;
+
+class UiRunner {
+    
+    @Karate.Test
+    Karate testUi() {
+    return Karate.run("ui").relativeTo(getClass());
+    }    
+
+}
