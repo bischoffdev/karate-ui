2530fc80ede890b509dd5457a0c897a01618efd0
Benjamin Bischoff <benjamin.bischoff@gmx.de>
2024-05-15 14:29:36 +0200

    local url

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -2,6 +2,7 @@ function fn() {
   karate.configure('driver', { type: 'chrome' });
 
   return {
-    baseUrl: 'https://coffee-cart.app/'
+    // baseUrl: 'https://coffee-cart.app/'
+    baseUrl: 'http://localhost:5173/'
   };
 }
